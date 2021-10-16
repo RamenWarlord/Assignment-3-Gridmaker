0caf981c72456c4d6c736e6c41b81246db0464bd
ramenwarlord <ivanyeung12345@gmail.com>
2021-10-15 20:42:59 -0400

    impl addR

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ let colorSelected;
 
 //Add a row
 function addR() {
-  alert("Clicked Add Row");
+  numRows++; //increment row count var
+  let grid = document.getElementById("grid"); //get grid
+  let prevRow = grid.lastElementChild; //get the last row of the grid
+  let row = document.createElement("tr"); //create a row
+  let col = document.createElement("td"); //create a col
+  if (prevRow == null) {
+    row.appendChild(col); //if there is no existing row, append one with one col
+    grid.appendChild(row);
+  } else {
+    for (let i = 0; i < prevRow.cells.length; i++) {
+      //otherwise append a row after appending the same # col as prev row
+      row.appendChild(col);
+    }
+    grid.appendChild(row);
+  }
 }
 //Add a column
 function addC() {
